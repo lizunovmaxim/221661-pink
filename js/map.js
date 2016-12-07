e81dcf41abe4466ee3a5bae18cebf445e2c2b0ae
Maxim Lizunov <lizunovwork@gmail.com>
2016-12-07 19:58:01 +0300

    add google maps script

---
+++ js/map.js
@@ -0,0 +1,25 @@
+function initMap() {
+  var mapPosition = {lat: 59.93635, lng: 30.32104};
+  var markerPosition = {lat: 59.93610, lng: 30.32104};
+
+  var map = new google.maps.Map(document.getElementById('contacts-map'), {
+    zoom: 16,
+    center: mapPosition,
+    zoomControl: true,
+    mapTypeControl: false,
+    scaleControl: false,
+    streetViewControl: false,
+    rotateControl: false,
+    scrollwheel: false
+  });
+
+  var marker = new google.maps.Marker({
+    position: markerPosition,
+    map: map,
+    icon: 'img/icon-map-marker.svg'
+  });
+
+  google.maps.event.addDomListener(window, 'resize', function() {
+    map.setCenter(mapPosition);
+  });
+}
